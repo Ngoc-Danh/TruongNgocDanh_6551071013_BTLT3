04cb640fe81dbacf6fdf0a9d0e725744bfb42277
Ngoc-Danh <danhtruong0704@gmail.com>
2026-09-07 11:10:50 +0700

    Add files via upload

---
+++ 6551071013_TruongNgocDanh_BTLT3/Bai 2/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Bai_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 163);
+            button1.ForeColor = Color.Red;
+            button1.Location = new Point(250, 122);
+            button1.Name = "button1";
+            button1.Size = new Size(288, 124);
+            button1.TabIndex = 0;
+            button1.Text = "Click Me";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+    }
+}
---
+++ 6551071013_TruongNgocDanh_BTLT3/Bai 2/Form1.cs
@@ -0,0 +1,25 @@
+namespace Bai_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Button was clicked.");
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hi , Welcome to C# 2010 programming!");
+        }
+    }
+}
